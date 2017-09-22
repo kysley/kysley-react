ffb7e8988214f13be955d3997de039c5efccf129
Evan Kysley <evan@kysley.com>
2017-09-22 14:33:56 -0400

    update 404

--- src/js/pages/PageNotFound.jsx
+++ src/js/pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageNotFound = () => (
   <div className="container">
-    <h1 className="fourohfour">It doesn't look like anything is here, friend :( </h1>
+    <h1 className="fourohfour fadeIn d-2">It doesn't look like anything is here, friend :(</h1>
   </div>
 )
 
