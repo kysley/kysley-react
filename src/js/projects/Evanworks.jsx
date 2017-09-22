6cac9215ad41986c46e22abc1e13d19cf0fd1c7f
Evan Kysley <evan@kysley.com>
2017-09-22 11:32:46 -0400

    add

---
+++ src/js/projects/Evanworks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import SVG from 'react-svg-inline'
+
+const Evanworks = () => (
+  <div>
+    <section className="container full">
+      <div className="col-12-of-12">
+        <img alt="" />
+      </div>
+    </section>
+  </div>
+)
+
+export default Evanworks
