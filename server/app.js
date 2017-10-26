28230bd33b82ab24ce05051b74e37b329c1c1490
Evan Kysley <evan@kysley.com>
2017-10-26 15:49:06 -0400

    server scaffolding, no more subdomain

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
-const subdomain = require('subdomain')
 
 const app = express()
 
+
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'))
 
-app.use(subdomain({ base: 'localhost', removeWWW: true }))
-
 app.use(express.static(path.resolve(__dirname, '..', 'dist')))
+app.use(express.static(path.resolve(__dirname)))
 
-app.get('/subdomain/:id/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, req.params.id, 'index.html'))
+// /tempest
+app.get('/tempest/', (req, res) => {
+  console.log(path.resolve(__dirname))
+  res.sendFile(path.resolve(__dirname, 'tempest', 'tempest.html'))
 })
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
 })
 
