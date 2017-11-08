7477701a6c42dc5abc4147b6b799d150363eab90
Evan Kysley <evan@kysley.com>
2017-11-08 16:04:15 -0500

    Prune unneeded paths

--- server/app.js
+++ server/app.js
@@ -10,36 +10,6 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 app.use(express.static(path.resolve(__dirname, '..', 'dist')))
 app.use(express.static(path.resolve(__dirname)))
 
-// /tempest
-app.get('/tempest/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'tempest', 'tempest.html'))
-})
-
-// /opus
-app.get('/opus/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'opus', 'opus.html'))
-})
-
-// /rgbhex
-app.get('/rgbhex/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'rgbhex', 'rgbhex.html'))
-})
-
-// /keepsake
-app.get('/keepsake/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'keepsake', 'keepsake.html'))
-})
-
-// /clock
-app.get('/clock/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'clock', 'clock.html'))
-})
-
-// /offshoreclothing
-app.get('/offshoreclothing/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'offshoreclothing', 'offshoreclothing.html'))
-})
-
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
 })
