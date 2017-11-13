226cf5896fec496e1fdbbbb140cb8fee9cfc5df8
Evan Kysley <evan@kysley.com>
2017-11-13 14:48:55 -0500

    change port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const app = require('./app')
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 8090
 
 app.listen(PORT, () => {
 	console.log(`App is listening on port ${PORT}!`)
